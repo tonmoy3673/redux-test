63ac360d4314f12bd119e3c9f0f95556efe55a69
tonmoy <tonmoy3673@gmail.com>
2023-07-18 01:00:54 +0600

    implement add to cart

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const initialProduct={
     count:2,
 };
 
-// ======= Action =========//
+//========== cart state ========//
+const initialCart={
+    cart:['Tomato'],
+    count:1,
+}
+
+// ======= Product Action =========//
 const getProduct=()=>{
     return{
         type:'GET_PRODUCT',
@@ -20,6 +26,21 @@ const addProduct =(product)=>{
         payload:product,
     }
 };
+
+// =========== Cart Action ========//
+const getCart=()=>{
+    return{
+        type:'GET_CART'
+    };
+};
+
+const addCart=(cart)=>{
+    return{
+        type:'ADD_CART',
+        payload:cart,
+    };
+};
+
 
 // =========== reducer ===========//
 const productReducer=(state=initialProduct,action)=>{
@@ -42,13 +63,38 @@ const productReducer=(state=initialProduct,action)=>{
 }
 
 
+// ========== Cart Reducer ==========//
+const cartReducer=(state=initialCart,action)=>{
+    switch (action.type) {
+        case 'GET_CART':
+            
+            return{
+                ...state,
+            }
+            case 'ADD_CART':
+            
+            return{
+                ...state,
+                cart:[...state.cart,action.payload],
+                count:state.count+1,
+            }
+
+    
+        default:
+            state;
+    }
+}
+
+
 // ======== dispatch =========//
 
-const store=createStore(productReducer);
+const store=createStore(cartReducer);
 store.subscribe(()=>{
     console.log(store.getState());
 });
 
-store.dispatch(getProduct());
-store.dispatch(addProduct('Mango'));
-store.dispatch(addProduct('Orange'));
+// store.dispatch(getProduct());
+// store.dispatch(addProduct('Mango'));
+// store.dispatch(addProduct('Orange'));
+store.dispatch(getCart());
+store.dispatch(addCart('Guava'));
