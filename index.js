b9224457b4d237b1c745c4e62dd81e63cbf9bd34
tonmoy <tonmoy3673@gmail.com>
2023-08-28 23:19:04 +0600

    set state

--- index.js
+++ index.js
@@ -1,54 +1,9 @@
-const { createStore } = require("redux");
-
-
+// ========== react counter app with redux ======//
 const initialState={
     count:0
-}
-
+};
 const addCount=()=>{
     return{
         type:'ADD'
     }
 };
-const removeCount=()=>{
-    return{
-        type:'REMOVE'
-    }
-};
-const resetCount=()=>{
-    return{
-        type:'RESET'
-    }
-};
-
-const countReducer=(state=initialState,action)=>{
-    switch (action.type) {
-        case 'ADD':
-        return{
-            ...state,
-            count:state.count+1
-        };
-        case 'REMOVE':
-            return{
-                ...state,
-                count:state.count-1
-            };
-            case 'RESET':
-                return{
-                    ...state,
-                    count:0
-                };
-    
-        default:
-            state;
-    }
-};
-const store=createStore(countReducer);
-store.subscribe(()=>{
-    console.log(store.getState())
-});
-
-store.dispatch(addCount());
-store.dispatch(addCount());
-store.dispatch(removeCount());
-store.dispatch(resetCount());
