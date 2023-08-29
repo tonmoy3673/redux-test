2cce794762360da4bc150805fc4ff980f8a34c8f
tonmoy <tonmoy3673@gmail.com>
2023-08-30 02:06:04 +0600

    set action

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ const initialStateCart={
     cart:['banana'],
     count:1,
 };
+
+const setProduct=(products)=>{
+    return{
+        type:'ADD',
+        payload:products,
+    }
+};
+const setCart=(cart)=>{
+    return{
+        type:'ADD',
+        payload:cart,
+    }
+};
