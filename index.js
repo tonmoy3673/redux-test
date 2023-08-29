59c5c1d8016cb6c844033e4e8203ee415caa89b5
tonmoy <tonmoy3673@gmail.com>
2023-08-30 02:03:29 +0600

    set initialState

--- index.js
+++ index.js
@@ -1,70 +1,8 @@
-const { createStore } = require("redux");
-
-// ========== react counter app with redux ======//
-const initialState={
-    count:0
+const initialStateProduct={
+    products:['mango','apple'],
+    count:1,
 };
-const addCount=()=>{
-    return{
-        type:'ADD'
-    }
+const initialStateCart={
+    cart:['banana'],
+    count:1,
 };
-const removeCount=()=>{
-    return{
-        type:'REMOVE'
-    }
-};
-const resetCount=()=>{
-    return{
-        type:'RESET'
-    }
-};
-
-const addBy5=(value)=>{
-    return{
-        type:'ADD5',
-       payload:value,
-    }
-}
-
-const countReducer=(state=initialState,action)=>{
-    switch (action.type) {
-        case 'ADD':
-        return{
-            ...state,
-            count:state.count+1
-        };
-        case 'REMOVE':
-        return{
-            ...state,
-            count:state.count-1
-        };
-        case 'RESET':
-        return{
-            ...state,
-            count:0
-        };
-         case 'ADD5':
-        return{
-            ...state,
-            count:state.count+action.payload
-            
-        };
-
-    
-        default:
-            state;
-    }
-};
-
-const store=createStore(countReducer);
-store.subscribe(()=>{
-    console.log(store.getState())
-});
-
-store.dispatch(addCount());
-store.dispatch(addCount());
-store.dispatch(removeCount());
-store.dispatch(resetCount());
-store.dispatch(addBy5(10));
-store.dispatch(addBy5(50));
