706313cd6882295e8d44fadb06ff55e337427325
tonmoy <tonmoy3673@gmail.com>
2023-08-31 23:46:39 +0600

    set actions

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ const initialState={
     todo:[],
     error:null
 };
+
+const getTodo=()=>{
+    return{
+        type:'GET'
+    }
+};
+
+const showTodo=(todo)=>{
+    return{
+        type:'SHOW',
+        payload:todo
+    }
+};
+
+const failTodo=(error)=>{
+    return{
+        type:'FAIL',
+        payload:error
+    }
+}
